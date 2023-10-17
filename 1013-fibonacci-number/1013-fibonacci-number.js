7f8619c8cf487cff5ee42e33e2af08eb87d79263
Muhammad Haider Bukhari <85192296+haiderBukhari@users.noreply.github.com>
2023-10-17 18:43:30 +0500

    Time: 44 ms (92.11%), Space: 41.9 MB (40.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -7,12 +7,9 @@ var fib = function(n) {
     if(n == 0 || n== 1){
         return n;
     }
-    if(!arr[n-1]){
-        arr[n-1] = fib(n-1);
-    }
-    if(!arr[n-2]){
-        arr[n-2] = fib(n-2);
-    }
     
+    if(!arr[n-1]) arr[n-1] = fib(n-1);
+    if(!arr[n-2]) arr[n-2] = fib(n-2);
+
     return arr[n-1] + arr[n-2];
 };
